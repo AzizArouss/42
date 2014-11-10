d305012da88a40548fc5795592f0e9af78afcd2a
Aziz AROUSS <aarouss@e2r12p1.42.fr>
2014-11-10 16:37:06 +0100

    Algo I - 42

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -6,11 +6,13 @@
 /*   By: aarouss <aarouss@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/08 16:05:26 by aarouss           #+#    #+#             */
-/*   Updated: 2014/11/08 17:50:00 by aarouss          ###   ########.fr       */
+/*   Updated: 2014/11/10 16:14:01 by aarouss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char *ft_itoa(int n)
-
+{
+	return (NULL);//
+}
--- Libft/ft_strsplit.c
+++ Libft/ft_strsplit.c
@@ -6,10 +6,13 @@
 /*   By: aarouss <aarouss@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/08 17:50:14 by aarouss           #+#    #+#             */
-/*   Updated: 2014/11/08 18:11:58 by aarouss          ###   ########.fr       */
+/*   Updated: 2014/11/10 16:12:07 by aarouss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char	**ft_strsplit(char const *s, char c)
+{
+	return (NULL);//
+}
