7019d47d8a2b8e79a63456b1a144b455ccabe7ec
Aziz AROUSS <aarouss@e2r11p5.42.fr>
2015-01-19 17:17:19 +0100

    Merlin a detruit ma journee

--- Infographie/fdf/ft_drawline.c
+++ Infographie/fdf/ft_drawline.c
@@ -6,7 +6,7 @@
 /*   By: aarouss <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/14 15:29:48 by aarouss           #+#    #+#             */
-/*   Updated: 2015/01/14 16:44:23 by aarouss          ###   ########.fr       */
+/*   Updated: 2015/01/19 16:26:28 by aarouss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,46 @@ void	ft_swapptr(t_pt **a, t_pt **b)
 	tmp = *a;
 	*a = *b;
 	*b = tmp;
+}
+
+void	ft_draw_first_case(t_pt *a, t_pt *b, t_env *e)
+{
+	double x;
+
+	x = a->x_2d;
+	while (x < b->x_2d)
+	{
+		ft_putpixel(x, a->y_2d + ((b->y_2d - a->y_2d) * (x - a->x_2d))
+				/ (b->x_2d - a->x_2d), e);
+		++x;
+	}
+}
+
+void	ft_draw_second_case(t_pt *a, t_pt *b, t_env *e)
+{
+	double y;
+
+	x = a->y_2d;
+	while (y < b->y_2d)
+	{
+		ft_putpixel(a->x_2d + ((b->x_2d - a->x_2d) * (y - a->y_2d))
+				/ (b->y_2d - a->y_2d), y, e);
+		++y;
+	}
+}
+
+void	ft_drawline(t_pt *a, t_pt *b, t_env *e)
+{
+	if (ABS(b->x_2d - a->x_2d) > ABS(b->y_2d - a->y_2d))
+	{
+		if (a->x_2d >= b->x_2d)
+			ft_swapptr(&a, &b);
+		ft_draw_first_case(a, b, e);
+	}
+	else
+	{
+		if (a->y_2d >= b->y_2d)
+			ft_swapptr(&a, &b);
+		ft_draw_second_case(a, b, e);
+	}
 }
---
+++ Infographie/fdf/ft_keypress.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_keypress.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aarouss <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/01/19 17:09:50 by aarouss           #+#    #+#             */
+/*   Updated: 2015/01/19 17:11:14 by aarouss          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+void	ft_colorchoice3(t_env *e, int keycode)
+{
+	if (keycode == 121 && (e->y = 0xcc))
+	{
+		e->b = 0;
+		e->g = 0xff;
+		e->r = 0xff;
+	}
+	if (keycode == 103 && (e->g = 0xcc))
+	{
+		e->r = 0x00;
+		e->b = 0x00;
+		e->g = 0xff;
+	}
+	if (keycode == 110 && (e->p = 0xcc))
+	{
+		e->r = 0;
+		e->b = 0;
+		e->g = 0;
+	}
+	ft_draw(e);
+}
+
+void	ft_colorchoice2(t_env *e, int keycode)
+{
+	if (keycode == 98 && (e->b = 0xe9))
+	{
+		e->r = 0x00;
+		e->g = 0x00;
+		e->b = 0xff;
+	}
+	if (keycode == 99 && (e->c = 0xcc))
+	{
+		e->r = 66;
+		e->b = 0;
+		e->g = 33;
+	}
+	ft_draw(e);
+}
+
+void	ft_colorchoice(t_env *e, int keycode)
+{
+	if (keycode == 32 || keycode == 100)
+	{
+		e->r = 0xcc;
+		e->g = 0xcc;
+		e->b = 0xcc;
+		e->y = 0xcc;
+		e->p = 0xcc;
+	}
+	if (keycode == 114 && (e->r = 0xcc))
+	{
+		e->g = 0;
+		e->b = 0;
+	}
+	if (keycode == 112 && (e->p = 0xcc))
+	{
+		e->g = 0;
+		e->b = 99;
+		e->r = 0xff;
+	}
+	ft_draw(e);
+}
+
+int		ft_exposehook(t_env *e)
+{
+	mlx_put_image_to_window(e->mlx, e->win, e->img, 0, 0);
+	return (0);
+}
+
+int		ft_keypress(int keycode, t_env *e)
+{
+	if (keycode == 65307)
+		exit(0);
+	if (keycode == 112 || keycode == 114 || keycode == 100)
+		ft_colorchoice(e, keycode);
+	if (keycode == 99 || keycode == 98)
+		ft_colorchoice2(e, keycode);
+	if (keycode == 121 || keycode == 103 || keycode == 110)
+		ft_colorchoice3(e, keycode);
+	ft_draw(e);
+	return (0);
+}
