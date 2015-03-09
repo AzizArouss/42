50dff3d74f16299e084fe3ed179d9b2ffe943ab0
Aziz AROUSS <aarouss@e1r7p1.42.fr>
2015-03-09 15:31:54 +0100

    Lol

---
+++ Infographie/wolf3d/libft/ft_strsub.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aarouss <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/01/06 12:32:01 by aarouss           #+#    #+#             */
+/*   Updated: 2015/01/30 14:51:16 by aarouss          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*str;
+
+	i = 0;
+	if (s == NULL)
+		return (NULL);
+	str = NULL;
+	str = ft_strnew(len);
+	if (str == NULL || start + len > ft_strlen(s))
+		return (NULL);
+	while (i < len + start)
+	{
+		if (i >= start)
+			str[i - start] = s[i];
+		i++;
+	}
+	return (str);
+}
